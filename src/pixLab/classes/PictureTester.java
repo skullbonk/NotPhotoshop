1bb54623bce2e7de88b4ad0df0b2c95b96d9a21e
Skullbonk <reagan.fairbanks@gmail.com>
2019-02-11 13:45:16 -0700

    Tried to create a method to glitch every frame of anya's snap from denny's, it will probably be catastrophic when I run it, but whatever.

--- src/pixLab/classes/PictureTester.java
+++ src/pixLab/classes/PictureTester.java
@@ -21,6 +21,7 @@ public class PictureTester
 {
 	public static ArrayList<String> images = new ArrayList<String>();
 	public static ArrayList<String> glitches = new ArrayList<String>();
+	public static ArrayList<Picture> dennysPngs = new ArrayList<Picture>();
 	
 	public static MathClass mathClass = new MathClass();
 	
@@ -41,7 +42,7 @@ public class PictureTester
 			
 			if(request == "help")
 			{
-				request = JOptionPane.showInputDialog(null, "options are: " + glitches.toString());
+				request = JOptionPane.showInputDialog(null, "options are: " + images.toString());
 			}
 			
 		return request;
@@ -160,7 +161,11 @@ public class PictureTester
 		while(!imageIsValid)
 		{
 			imageToGlitch = imageSelector();
-			if(images.contains(imageToGlitch))
+			if(imageSelector().equals("dennys"))
+			{
+				imageIsValid = true;
+			}
+			else if(images.contains(imageToGlitch))
 			{
 				imageIsValid = true;
 			}
@@ -171,26 +176,46 @@ public class PictureTester
 			}
 		}
 		
-		image = new Picture(imageToGlitch);
-		
-		image.explore();
-		
-		glitch = glitchSelector();
-		
-		for(int glitchIndex = 0; glitchIndex < glitches.size(); glitchIndex++)
+		if(imageToGlitch.equals("dennys"))
 		{
-			if(glitches.get(glitchIndex).equals(glitch))
+			String indexToString;
+			for(int index = 0; index < 169; index ++)
 			{
-				glitchToRun = glitch;
-				System.out.println("glitch successfully selected");
+				indexToString = "dennys snap" + Integer.toString(index) + ".png";
+				image = new Picture(indexToString);
+				dennysPngs.add(image);
 			}
+			glitch = glitchSelector();
+			
+			for(int dennysPngIndex = 0; dennysPngIndex < 169; dennysPngIndex ++)
+			{
+				
+			}
+			
 		}
-		
-		System.out.println(glitchToRun);
-		runGlitch(glitch);
-		System.out.println("glitch " + glitchToRun);
-		image.explore();
+		else
+		{
+			image = new Picture(imageToGlitch);
+			
+			image.explore();
+			
+			glitch = glitchSelector();
+			
+			for(int glitchIndex = 0; glitchIndex < glitches.size(); glitchIndex++)
+			{
+				if(glitches.get(glitchIndex).equals(glitch))
+				{
+					glitchToRun = glitch;
+					System.out.println("glitch successfully selected");
+				}
+			}
+			
+			System.out.println(glitchToRun);
+			runGlitch(glitch);
+			System.out.println("glitch " + glitchToRun);
+			image.explore();
 //		save(image);
+		}
 	}
 	
 	
@@ -199,101 +224,240 @@ public class PictureTester
 	{
 		String method = glitch;
 		
-		if(method.equals(glitches.get(0)))
+		if(method.contains("dennys"))
 		{
-			System.out.println("Running glitch " + method);
-			image.zeroBlue();
+			if(method.contains(glitches.get(0)))
+			{
+				System.out.println("Running glitch " + method);
+				for(int index = 0; index < 169; index ++)
+				{
+					image = dennysPngs.get(index);
+					image.zeroBlue();
+					image.explore();
+				}
+			}
+			if(method.contains(glitches.get(1)))
+			{
+				System.out.println("Running glitch " + method);
+				for(int index = 0; index < 169; index ++)
+				{
+					image = dennysPngs.get(index);
+					image.mirrorVertical();
+					image.explore();
+				}
+			}
+			if(method.contains(glitches.get(2)))
+			{
+				System.out.println("Running glitch " + method);
+				for(int index = 0; index < 169; index ++)
+				{
+					image = dennysPngs.get(index);
+					image.mirrorTemple();
+					image.explore();
+				}
+			}
+			if(method.contains(glitches.get(3)))
+			{
+				System.out.println("Running glitch " + method);
+				for(int index = 0; index < 169; index ++)
+				{
+					image = dennysPngs.get(index);
+					image.mirrorGull();
+					image.explore();
+				}
+			}
+			if(method.contains(glitches.get(4)))
+			{
+				System.out.println("Running glitch " + method);
+				for(int index = 0; index < 169; index ++)
+				{
+					image = dennysPngs.get(index);
+					image.glitchGang();
+					image.explore();
+				}
+			}
+			if(method.contains(glitches.get(5)))
+			{
+				System.out.println("Running glitch " + method);
+				for(int index = 0; index < 169; index ++)
+				{
+					image = dennysPngs.get(index);
+					image.fizzle();
+					image.explore();
+				}
+			}
+			if(method.contains(glitches.get(6)))
+			{
+				System.out.println("Running glitch " + method);
+				for(int index = 0; index < 169; index ++)
+				{
+					image = dennysPngs.get(index);
+					image.fizzleRemastered();
+					image.explore();
+				}
+			}
+			if(method.contains(glitches.get(8)))
+			{
+				System.out.println("Running glitch " + method);
+				for(int index = 0; index < 169; index ++)
+				{
+					image = dennysPngs.get(index);
+					image.revealPicture();
+					image.explore();
+				}
+			}
+			if(method.contains("math class"))
+			{
+				System.out.println("Running glitch " + method);
+				for(int index = 0; index < 169; index ++)
+				{
+					image = dennysPngs.get(index);
+					image.mathClass();
+					image.explore();
+				}
+			}
+			if(method.contains("take chunks"))
+			{
+				System.out.println("Running glitch " + method);
+				for(int index = 0; index < 169; index ++)
+				{
+					image = dennysPngs.get(index);
+					image.takeChunks();
+					image.explore();
+				}
+			}
+			if(method.contains("only highest"))
+			{
+				System.out.println("Running glitch " + method);
+				for(int index = 0; index < 169; index ++)
+				{
+					image = dennysPngs.get(index);
+					image.onlyHighest();
+					image.explore();
+				}
+			}
+			if(method.contains("destruc"))
+			{
+				System.out.println("Running glitch " + method);
+				for(int index = 0; index < 169; index ++)
+				{
+					image = dennysPngs.get(index);
+					image.destruc();
+					image.explore();
+				}
+			}
+			if(method.contains("sweep"))
+			{
+				System.out.println("Running glitch " + method);
+				for(int index = 0; index < 169; index ++)
+				{
+					image = dennysPngs.get(index);
+					image.sweep();
+					image.explore();
+				}
+			}
 		}
 		
-
-		if(method.equals(glitches.get(1)))
-		{
-			System.out.println("Running glitch " + method);
-			image.mirrorVertical();
-		}
-		
-
-		if(method.equals(glitches.get(2)))
-		{
-			System.out.println("Running glitch " + method);
-			image.mirrorTemple();
-		}
-		
-
-		if(method.equals(glitches.get(3)))
-		{
-			System.out.println("Running glitch " + method);
-			image.mirrorGull();
-		}
-		
-
-		if(method.equals(glitches.get(4)))
-		{
-			System.out.println("Running glitch " + method);
-			image.glitchGang();
-		}
-		
-
-		if(method.equals(glitches.get(5)))
-		{
-			System.out.println("Running glitch " + method);
-			image.fizzle();
-		}
-		
-
-		if(method.equals(glitches.get(6)))
-		{
-			System.out.println("Running glitch " + method);
-			image.fizzleRemastered();
-		}
-		
-		
-
-/*
+		else
+		{	
+			if(method.equals(glitches.get(0)))
+			{
+				System.out.println("Running glitch " + method);
+				image.zeroBlue();
+			}
+			
+			
+			if(method.equals(glitches.get(1)))
+			{
+				System.out.println("Running glitch " + method);
+				image.mirrorVertical();
+			}
+			
+			
+			if(method.equals(glitches.get(2)))
+			{
+				System.out.println("Running glitch " + method);
+				image.mirrorTemple();
+			}
+			
+			
+			if(method.equals(glitches.get(3)))
+			{
+				System.out.println("Running glitch " + method);
+				image.mirrorGull();
+			}
+			
+			
+			if(method.equals(glitches.get(4)))
+			{
+				System.out.println("Running glitch " + method);
+				image.glitchGang();
+			}
+			
+			
+			if(method.equals(glitches.get(5)))
+			{
+				System.out.println("Running glitch " + method);
+				image.fizzle();
+			}
+			
+			
+			if(method.equals(glitches.get(6)))
+			{
+				System.out.println("Running glitch " + method);
+				image.fizzleRemastered();
+			}
+			
+			
+			
+			
+			
+			/*
 		if(method.equals(glitches.get(7)))
 		{
 			image.hidePicture();
 		}
-*/		
-		
-		if(method.equals(glitches.get(8)))
-		{
-			System.out.println("Running glitch " + method);
-			image.revealPicture();
+			 */		
+			
+			if(method.equals(glitches.get(8)))
+			{
+				System.out.println("Running glitch " + method);
+				image.revealPicture();
+			}
+			
+			
+			if(method.equals("math class"))
+			{
+				System.out.println("Running glitch " + method);
+				image.mathClass();
+			}
+			
+			if(method.equals("take chunks"))
+			{
+				System.out.println("Running glitch " + method);
+				image.takeChunks();
+			}
+			
+			if(method.equals("only highest"))
+			{
+				System.out.println("Running glitch " + method);
+				image.onlyHighest();
+			}
+			
+			if(method.equals("destruc"))
+			{
+				System.out.println("Running glitch " + method);
+				image.destruc();
+			}
+			
+			if(method.equals("sweep"))
+			{
+				System.out.println("Running glitch " + method);
+				image.sweep();
+			}
+			
+			System.out.println("complete");
 		}
-		
-
-		if(method.equals("math class"))
-		{
-			System.out.println("Running glitch " + method);
-			image.mathClass();
-		}
-		
-		if(method.equals("take chunks"))
-		{
-			System.out.println("Running glitch " + method);
-			image.takeChunks();
-		}
-		
-		if(method.equals("only highest"))
-		{
-			System.out.println("Running glitch " + method);
-			image.onlyHighest();
-		}
-		
-		if(method.equals("destruc"))
-		{
-			System.out.println("Running glitch " + method);
-			image.destruc();
-		}
-		
-		if(method.equals("sweep"))
-		{
-			System.out.println("Running glitch " + method);
-			image.sweep();
-		}
-		
-		System.out.println("complete");
 	}
 	
 	public static void save(Picture image)
@@ -308,6 +472,14 @@ public class PictureTester
 		}
 	}
 	
+	
+	public static void testDennys()
+	{
+		for(int index = 0; index < 169; index++)
+		{
+			
+		}
+	}
 	
   /** Method to test zeroBlue */
   public static void testZeroBlue()
