b449a1995d5edf1f12535304a2d1bffe23e32723
Skullbonk <reagan.fairbanks@gmail.com>
2019-02-22 13:14:46 -0700

    deleted the entire layout cause it didn't even work

--- src/pixLab/classes/PickerFrame.java
+++ src/pixLab/classes/PickerFrame.java
@@ -4,6 +4,7 @@ import java.awt.GridBagLayout;
 import pixLab.classes.PicturePicker;
 import pixLab.classes.PictureTester;
 import javax.swing.JFrame;
+import javax.swing.SpringLayout;
 
 public class PickerFrame extends JFrame
 {
@@ -14,12 +15,9 @@ public class PickerFrame extends JFrame
 	{
 		super();
 		this.pickPanel = new PickerPanel(picker);
-		GridBagLayout gbl_pickPanel = new GridBagLayout();
-		gbl_pickPanel.columnWidths = new int[]{0};
-		gbl_pickPanel.rowHeights = new int[]{0};
-		gbl_pickPanel.columnWeights = new double[]{Double.MIN_VALUE};
-		gbl_pickPanel.rowWeights = new double[]{Double.MIN_VALUE};
-		pickPanel.setLayout(gbl_pickPanel);
+		SpringLayout pickerLayout = new SpringLayout();
+		
+		pickPanel.setLayout(pickerLayout);
 		
 		setupFrame();
 	}
--- src/pixLab/classes/PickerPanel.java
+++ src/pixLab/classes/PickerPanel.java
@@ -8,6 +8,9 @@ import java.awt.Canvas;
 import java.awt.Dimension;
 import java.awt.Component;
 import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class PickerPanel extends JPanel
 {
@@ -34,7 +37,7 @@ public class PickerPanel extends JPanel
 	
 	private JButton saveButton;
 	
-	private SpringLayout greatLayout;
+	private SpringLayout pickerLayout;
 	
 	public PickerPanel(PicturePicker picker)
 	{
@@ -42,26 +45,24 @@ public class PickerPanel extends JPanel
 		
 		this.picker = picker;
 		
-		loadButton = new JButton("load an image");
-
-		picPathLabel = new JLabel("Image: " + picPath);
-		picPathLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		picPathLabel.setFont(new Font("Courier", Font.PLAIN, 55));
-		picPathLabel.setForeground(Color.WHITE);
+		this.loadButton = new JButton("load");
+		this.zeroBlueButton = new JButton("zero blue");
+		this.mirrorVerticalButton = new JButton("mirror vertical");
+		this.glitchGangButton = new JButton("glitch gang");
+		this.fizzleButton = new JButton("fizzle");
+		this.fizzleRemasteredButton = new JButton("fizzle remastered");
+		this.hidePictureButton = new JButton("hide picture");
+		this.revealPictureButton = new JButton("reveal picture");
+		this.mathClassButton = new JButton("math class");
+		this.takeChunksButton = new JButton("take chunks");
+		this.onlyHighestButton = new JButton("only highest");
+		this.destrucButton = new JButton("destruc");
+		this.sweepButton = new JButton("sweep");
 		
-		saveButton = new JButton("Save image");
-		
-		greatLayout = new SpringLayout();
-		
-		
-		
-		
-		
-//		setButtonTips();
+		pickerLayout = new SpringLayout();
 		setupPanel();
 		setupLayout();
-		setupListeners();
-		
+		setupListeners();		
 	}
 	
 //	public void setButtonTips()
@@ -83,147 +84,31 @@ public class PickerPanel extends JPanel
 
 	public void setupPanel()
 	{
-		this.setLayout(greatLayout);
-		
+		this.setLayout(pickerLayout);
 		this.setPreferredSize(new Dimension(800, 600));
 		this.setBackground(Color.DARK_GRAY);
 		
 		this.add(loadButton);
-		this.add(saveButton);
+		
 		this.add(picPathLabel);
+		this.add(saveButton);
+		
+		this.add(zeroBlueButton);
+		this.add(mirrorVerticalButton);
+		this.add(glitchGangButton);
+		this.add(fizzleButton);
+		this.add(fizzleRemasteredButton);
+		this.add(hidePictureButton);
+		this.add(revealPictureButton);
+		this.add(takeChunksButton);
+		this.add(onlyHighestButton);
+		this.add(destrucButton);
+		this.add(sweepButton);
 	}
 	
 	public void setupLayout()
 	{
-		greatLayout.putConstraint(SpringLayout.NORTH, saveButton, 263, SpringLayout.NORTH, this);
-		greatLayout.putConstraint(SpringLayout.NORTH, picPathLabel, 197, SpringLayout.NORTH, this);
-		greatLayout.putConstraint(SpringLayout.SOUTH, picPathLabel, -343, SpringLayout.SOUTH, this);
-		greatLayout.putConstraint(SpringLayout.NORTH, loadButton, 0, SpringLayout.NORTH, saveButton);
-		greatLayout.putConstraint(SpringLayout.WEST, loadButton, 0, SpringLayout.WEST, picPathLabel);
-		greatLayout.putConstraint(SpringLayout.EAST, loadButton, -414, SpringLayout.EAST, this);
-		greatLayout.putConstraint(SpringLayout.WEST, saveButton, 424, SpringLayout.WEST, this);
-		greatLayout.putConstraint(SpringLayout.EAST, saveButton, 0, SpringLayout.EAST, picPathLabel);
-		greatLayout.putConstraint(SpringLayout.WEST, picPathLabel, 206, SpringLayout.WEST, this);
-		greatLayout.putConstraint(SpringLayout.EAST, picPathLabel, -206, SpringLayout.EAST, this);
 		
-		Component verticalGlue = Box.createVerticalGlue();
-		greatLayout.putConstraint(SpringLayout.WEST, verticalGlue, 0, SpringLayout.WEST, this);
-		greatLayout.putConstraint(SpringLayout.EAST, verticalGlue, 0, SpringLayout.WEST, this);
-		add(verticalGlue);
-		
-		JLabel lblAvailableGlitches = new JLabel("Available glitches:");
-		greatLayout.putConstraint(SpringLayout.NORTH, lblAvailableGlitches, 23, SpringLayout.SOUTH, loadButton);
-		greatLayout.putConstraint(SpringLayout.WEST, lblAvailableGlitches, 0, SpringLayout.WEST, loadButton);
-		greatLayout.putConstraint(SpringLayout.SOUTH, lblAvailableGlitches, 52, SpringLayout.SOUTH, loadButton);
-		greatLayout.putConstraint(SpringLayout.EAST, lblAvailableGlitches, 594, SpringLayout.WEST, this);
-		lblAvailableGlitches.setFont(new Font("Courier", Font.ITALIC, 25));
-		lblAvailableGlitches.setHorizontalAlignment(SwingConstants.CENTER);
-		lblAvailableGlitches.setBackground(Color.DARK_GRAY);
-		lblAvailableGlitches.setForeground(Color.WHITE);
-		add(lblAvailableGlitches);
-		
-		Box horizontalBox = Box.createHorizontalBox();
-		greatLayout.putConstraint(SpringLayout.NORTH, horizontalBox, 26, SpringLayout.SOUTH, lblAvailableGlitches);
-		greatLayout.putConstraint(SpringLayout.SOUTH, horizontalBox, -182, SpringLayout.SOUTH, this);
-		greatLayout.putConstraint(SpringLayout.WEST, horizontalBox, 100, SpringLayout.WEST, this);
-		greatLayout.putConstraint(SpringLayout.EAST, horizontalBox, -100, SpringLayout.EAST, this);
-		add(horizontalBox);
-		
-		zeroBlueButton = new JButton("zero blue");
-		zeroBlueButton.setToolTipText("deletes blue");
-		zeroBlueButton.setBackground(Color.LIGHT_GRAY);
-		horizontalBox.add(zeroBlueButton);
-		greatLayout.putConstraint(SpringLayout.WEST, zeroBlueButton, 100, SpringLayout.WEST, this);
-		mirrorVerticalButton = new JButton("mirror vertical");
-		mirrorVerticalButton.setToolTipText("mirrors vertically");
-		horizontalBox.add(mirrorVerticalButton);
-		glitchGangButton = new JButton("glitch gang");
-		glitchGangButton.setToolTipText("I forgot what this does, but I bet it's boring and broken");
-		horizontalBox.add(glitchGangButton);
-		fizzleButton = new JButton("fizzle");
-		fizzleButton.setToolTipText("The original glitch - simultaneously boring and interesting");
-		horizontalBox.add(fizzleButton);
-		fizzleRemasteredButton = new JButton("fizzle - remastered");
-		fizzleRemasteredButton.setToolTipText("Fizzle's big brother that was actually born later somehow, but we'll ignore that");
-		horizontalBox.add(fizzleRemasteredButton);
-		
-		Box horizontalBox_1 = Box.createHorizontalBox();
-		greatLayout.putConstraint(SpringLayout.NORTH, horizontalBox_1, 424, SpringLayout.NORTH, this);
-		greatLayout.putConstraint(SpringLayout.WEST, horizontalBox_1, 150, SpringLayout.WEST, this);
-		greatLayout.putConstraint(SpringLayout.SOUTH, horizontalBox_1, 54, SpringLayout.SOUTH, horizontalBox);
-		greatLayout.putConstraint(SpringLayout.EAST, horizontalBox_1, -50, SpringLayout.EAST, horizontalBox);
-		add(horizontalBox_1);
-		hidePictureButton = new JButton("hide picture");
-		hidePictureButton.setToolTipText("Hide a picture in another picture, probably doesn't work");
-		horizontalBox_1.add(hidePictureButton);
-		revealPictureButton = new JButton("reveal picture");
-		revealPictureButton.setToolTipText("Reveals a hidden picture, probably doesn't work");
-		horizontalBox_1.add(revealPictureButton);
-		mathClassButton = new JButton("math class");
-		takeChunksButton = new JButton("take chunks");
-		takeChunksButton.setToolTipText("Turns everything into fuzzy squares and does some whacky purple red shiz");
-		horizontalBox_1.add(takeChunksButton);
-		onlyHighestButton = new JButton("only highest");
-		onlyHighestButton.setToolTipText("Deletes every color but the highest one. Boring, but could be useful");
-		horizontalBox_1.add(onlyHighestButton);
-		
-		Box horizontalBox_2 = Box.createHorizontalBox();
-		greatLayout.putConstraint(SpringLayout.NORTH, horizontalBox_2, 6, SpringLayout.SOUTH, horizontalBox_1);
-		greatLayout.putConstraint(SpringLayout.WEST, horizontalBox_2, 250, SpringLayout.WEST, this);
-		greatLayout.putConstraint(SpringLayout.SOUTH, horizontalBox_2, 54, SpringLayout.SOUTH, horizontalBox_1);
-		greatLayout.putConstraint(SpringLayout.EAST, horizontalBox_2, -278, SpringLayout.EAST, this);
-		add(horizontalBox_2);
-		destrucButton = new JButton("destruc");
-		destrucButton.setToolTipText("I forgot what this does, but it's probably underwhelming");
-		horizontalBox_2.add(destrucButton);
-		greatLayout.putConstraint(SpringLayout.WEST, destrucButton, 244, SpringLayout.WEST, this);
-		greatLayout.putConstraint(SpringLayout.SOUTH, destrucButton, -10, SpringLayout.SOUTH, this);
-		sweepButton = new JButton("sweep");
-		sweepButton.setToolTipText("Crisps it up and also makes phantoms in the corner");
-		horizontalBox_2.add(sweepButton);
-		greatLayout.putConstraint(SpringLayout.WEST, sweepButton, 359, SpringLayout.WEST, this);
-		greatLayout.putConstraint(SpringLayout.SOUTH, sweepButton, 0, SpringLayout.SOUTH, this);
-		
-		Component horizontalStrut_2 = Box.createHorizontalStrut(20);
-		greatLayout.putConstraint(SpringLayout.WEST, horizontalStrut_2, 0, SpringLayout.WEST, verticalGlue);
-		greatLayout.putConstraint(SpringLayout.SOUTH, horizontalStrut_2, -196, SpringLayout.SOUTH, this);
-		greatLayout.putConstraint(SpringLayout.EAST, horizontalStrut_2, -6, SpringLayout.WEST, horizontalBox);
-		add(horizontalStrut_2);
-		
-		Component horizontalStrut_3 = Box.createHorizontalStrut(20);
-		greatLayout.putConstraint(SpringLayout.NORTH, horizontalStrut_3, 0, SpringLayout.NORTH, horizontalStrut_2);
-		greatLayout.putConstraint(SpringLayout.WEST, horizontalStrut_3, 6, SpringLayout.EAST, horizontalBox);
-		greatLayout.putConstraint(SpringLayout.SOUTH, horizontalStrut_3, -210, SpringLayout.SOUTH, this);
-		greatLayout.putConstraint(SpringLayout.EAST, horizontalStrut_3, 0, SpringLayout.EAST, this);
-		add(horizontalStrut_3);
-		
-		Component horizontalStrut_4 = Box.createHorizontalStrut(20);
-		greatLayout.putConstraint(SpringLayout.WEST, horizontalStrut_4, 0, SpringLayout.WEST, this);
-		greatLayout.putConstraint(SpringLayout.EAST, horizontalStrut_4, -6, SpringLayout.WEST, lblAvailableGlitches);
-		greatLayout.putConstraint(SpringLayout.NORTH, horizontalStrut_2, 46, SpringLayout.SOUTH, horizontalStrut_4);
-		greatLayout.putConstraint(SpringLayout.NORTH, horizontalStrut_4, -28, SpringLayout.SOUTH, lblAvailableGlitches);
-		greatLayout.putConstraint(SpringLayout.SOUTH, horizontalStrut_4, 0, SpringLayout.SOUTH, lblAvailableGlitches);
-		add(horizontalStrut_4);
-		
-		Component horizontalStrut_5 = Box.createHorizontalStrut(20);
-		greatLayout.putConstraint(SpringLayout.NORTH, horizontalStrut_5, 328, SpringLayout.NORTH, this);
-		greatLayout.putConstraint(SpringLayout.WEST, horizontalStrut_5, 6, SpringLayout.EAST, lblAvailableGlitches);
-		greatLayout.putConstraint(SpringLayout.EAST, horizontalStrut_5, 0, SpringLayout.EAST, this);
-		add(horizontalStrut_5);
-		
-		Component glue = Box.createGlue();
-		greatLayout.putConstraint(SpringLayout.NORTH, glue, 0, SpringLayout.NORTH, loadButton);
-		greatLayout.putConstraint(SpringLayout.WEST, glue, 32, SpringLayout.EAST, loadButton);
-		greatLayout.putConstraint(SpringLayout.SOUTH, glue, -6, SpringLayout.NORTH, lblAvailableGlitches);
-		greatLayout.putConstraint(SpringLayout.EAST, glue, -40, SpringLayout.WEST, saveButton);
-		add(glue);
-		
-		Component rigidArea_1 = Box.createRigidArea(new Dimension(20, 20));
-		greatLayout.putConstraint(SpringLayout.NORTH, rigidArea_1, 10, SpringLayout.NORTH, this);
-		greatLayout.putConstraint(SpringLayout.WEST, rigidArea_1, -200, SpringLayout.EAST, this);
-		greatLayout.putConstraint(SpringLayout.SOUTH, rigidArea_1, 0, SpringLayout.SOUTH, loadButton);
-		greatLayout.putConstraint(SpringLayout.EAST, rigidArea_1, 0, SpringLayout.EAST, this);
-		add(rigidArea_1);
 	}
 	
 	public void setupListeners()
